81f18e3c76cd636c8d923ff040c6ba7babfd6c98
woosoojin <wsj1998@naver.com>
2022-04-10 15:11:08 +0900

    chap02

---
+++ chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,10 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		int value = 5;
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+}
