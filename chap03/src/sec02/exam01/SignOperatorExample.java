d62ce85942055f88c20fe54d2587b5e28bae78e8
woosoojin <wsj1998@naver.com>
2022-04-11 22:06:28 +0900

    operator

---
+++ chap03/src/sec02/exam01/SignOperatorExample.java
@@ -0,0 +1,16 @@
+package sec02.exam01;
+
+public class SignOperatorExample {
+	public static void main(String[] args) {
+		int x = -100;
+		int result1 = +x;
+		int result2 = -x;
+		System.out.println("result1=" + result1);
+		System.out.println("result2=" + result2);
+		
+		byte b = 100;
+		// byte result3 = -b;
+		int resutl3 = -b;
+		System.out.println("result3=" + resutl3);
+	}
+}
